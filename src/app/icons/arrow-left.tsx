020b7ed933c492b97892d6955846d61a245b9021
jlee-3 <leejhnthn93@gmail.com>
2023-12-20 15:01:12 +0800

    feat: add icons

---
+++ src/app/icons/arrow-left.tsx
@@ -0,0 +1,24 @@
+export default function ArrowLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <path
+        d="M12.6667 8H3.33334"
+        stroke="#2A2B38"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.00001 12.6667L3.33334 8.00001L8.00001 3.33334"
+        stroke="#2A2B38"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
